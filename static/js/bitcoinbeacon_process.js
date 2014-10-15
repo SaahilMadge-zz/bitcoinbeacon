f59e316e0faf479b58466e2793b93cdbc4c3021e
SaahilM <smadge@princeton.edu>
2014-10-15 13:47:11 -0400

    Prototype random extraction

--- static/js/bitcoinbeacon_process.js
+++ static/js/bitcoinbeacon_process.js
@@ -1,5 +1,6 @@
 var confirmButton = $('#confirmManifestButton');
 var file = $('#manifestUpload')[0];
+var blockBaseURL = "http://blockchain.info/block-index/$block_index?format=json"
 
 function printFileInfo(file)
 {
@@ -31,6 +32,7 @@ function readFile()
 	var manifestJSON;
 	reader.onload = function()
 	{
+		// get the JSON fields
 		manifestJSON = JSON.parse(reader.result);
 		console.log(manifestJSON);
 		blockNumber = manifestJSON.futureBlockNum;
@@ -38,7 +40,47 @@ function readFile()
 		numParticipants = lotteryDetails.numParticipants;
 		numWinners = lotteryDetails.numWinners;
 		randomKey = manifestJSON.randomKey;
-		console.log("blockNumber: " + blockNumber + ", numParticipants: " + numParticipants + ", numWinners: " + numWinners);
+		console.log("blockNumber: " + blockNumber + ", numParticipants: " + numParticipants + ", numWinners: " + numWinners + ", randomKey: " + randomKey);
+
+		// convert the random key to hex
+		randomKeyHex = randomKey.toString(16);
+		console.log("randomKey to array: " + randomKeyHex);
+
+		// convert the hex to bitArray for use in sjcl
+		randomKeyBitArray = sjcl.codec.hex.toBits(randomKeyHex);
+		console.log(randomKeyBitArray);
+
+		// query blockchain.info for the hash of that block
+		urlReplacedCORS = blockBaseURL.replace("$block_index", blockNumber) + "?cors=true";
+		console.log(urlReplacedCORS);
+
+		// $.getJSON(urlReplaced + "?callback=?", null, function(data)
+		// {
+		// 	var blockJSONData = JSON.parse(data)
+		// 	console.log(blockJSONData);
+		// });
+
+		// $.ajax(
+		// {
+		// 	type: "GET",
+		// 	dataType: "json",
+		// 	url: urlReplacedCORS,
+		// 	crossDomain:true,
+		// 	contentType: 'text/plain'
+		// }).done(function(json)
+		// {
+		// 	latestblock = json;
+		// 	console.log(json);
+		// }).fail(function(textStatus, error)
+		// {
+		// 	console.log("Error: " + textStatus + " " + error);
+		// });
+
+		var blockHash = "000000000000085a032822ec22783149b3af4d590634038c7dfe217f2b3c68bf";
+		var hmacOut = new sjcl.misc.hmac(randomKeyBitArray).encrypt(blockHash);
+		var hmacOut32 = sjcl.bitArray.clamp(hmacOut, 32);
+		console.log("hmacOut32: " + hmacOut32);
+		console.log("hmacOut32Length: " + sjcl.bitArray.bitLength(hmacOut32));
 	}
 	reader.readAsText(file);	
 }
